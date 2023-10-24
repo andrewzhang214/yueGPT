b9b12d8e3c97050c55a7f4b0bd37c14b1a953753
Andrew Zhang <andrewzhang@Andrews-MBP.attlocal.net>
2023-10-24 16:09:42 -0700

    added argparser

--- generate.py
+++ generate.py
@@ -1,37 +1,33 @@
-import os
-
 import torch
-import torch.nn as nn
-from torch.nn import functional as F
-
+import argparse
 from yueGPT import yueGPT
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-# Read txt file into string
-text = ""
-with open('data/text.txt', 'r', encoding='utf-8') as file:
-    text = file.read()
-
-# Get token list and vocab_size
-tokenList = set(text)
-VOCAB_SIZE = len(tokenList)
-
-# Create a decoder/encoder
-# This is in lieu of having a proper tokenizer, since we are treating each character
-# as an individual token
-stoi = {t:i for i, t in enumerate(sorted(tokenList))}
-itos = {i:t for i, t in enumerate(sorted(tokenList))}
-encode = lambda s: [stoi[t] for t in s]
-decode = lambda t: [itos[i] for i in t]
 
 
 def __main__():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # Read txt file into string
+    text = ""
+    with open('data/text.txt', 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    # Get token list and vocab_size
+    tokenList = set(text)
+
+    # Create a decoder/encoder
+    # This is in lieu of having a proper tokenizer, since we are treating each character
+    # as an individual token
+    stoi = {t:i for i, t in enumerate(sorted(tokenList))}
+
     m = yueGPT().to(device=device)
     m.load_state_dict(torch.load('checkpoints/yueGPT28M.pt', map_location=torch.device('cpu')))
     startWord = (torch.ones((1,1), dtype=torch.long) * stoi[' ']).to(device=device)
-    m.generate(startWord)
+    m.generate(startWord, max_new_tokens=args.length)
 
 
+ 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate text from yueGPT')
+    parser.add_argument('-l', '--length', help="Length of text to generate", type=int)
+    args = parser.parse_args()
     __main__()
